2bb72340f1c2d8667893bc381dfe637421f1df85
ssysshy <foreverzrd@icloud.com>
2017-10-31 15:25:29 +0800

    build v1.0.4 common

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -6,7 +6,7 @@
         <Button slot="prepend" @click="onDeletes">删除所选记录</Button>
         <Button slot="append" icon="ios-search" @click="onSearch"></Button>
         </Input>
-        <Table border :columns="columns7"
+        <Table border :columns="columns"
                :data="filter.list"
                @on-selection-change="onSelectionChange"
         ></Table>
@@ -54,10 +54,13 @@
     </div>
 </template>
 <script>
+    import Base from '../../common/base';
     export default {
+        mixins:[Base],
         data () {
             return {
-                columns7: [
+                module:'users',
+                columns: [
                     {
                         type: 'selection',
                         width: 60,
@@ -129,17 +132,6 @@
                         }
                     }
                 ],
-                ids: [],
-                filter: {
-                    list: [],
-                    total: 0,
-                    page: 1,
-                    rows: 10,  //每页限制条数
-                    name: '',
-                    id: ''
-                },
-                modal: false,
-
 
                 formValidate: {
                     name: '',
@@ -163,140 +155,5 @@
                 }
             }
         },
-
-        methods: {
-            onAdd(){
-                this.$refs['formValidate'].resetFields();
-                delete this.formValidate._id;
-                this.modal = true;
-            },
-            show (params) {
-                console.log(params);
-                this.modal = true;
-                Object.assign(this.$data.formValidate,params.row)
-            },
-            getData(){
-                this.$http.post('http://localhost:3000/users/list', this.filter)
-                    .then(res => {
-                        this.filter.list = res.data.rows;
-                        this.filter.page = res.data.page;
-                        this.filter.total = res.data.total;
-                    })
-            },
-            remove (index, id) {
-
-                this.$Modal.confirm({
-                    title: '确认删除该条数据',
-                    content: '<p>删除将无法恢复</p>',
-                    onOk: () => {
-                        this.filter.list.splice(index, 1);
-//                console.log(id);
-                        this.filter.id = id;
-                        this.$http.delete('http://localhost:3000/users/data/' + this.filter.id)
-                            .then(res => {
-                                this.$Message.info('删除成功');
-                            })
-                    },
-                    onCancel: () => {
-                        this.$Message.info('取消删除');
-                    }
-                });
-
-            },
-            onChangePage(page){
-                console.log(page);
-                this.filter.page = page;
-                this.getData();
-            },
-            onChangePageSize(size){
-
-                this.filter.rows = size;
-                this.getData();
-            },
-            onSearch(){
-                this.getData();
-            },
-            onDeletes(){
-                if (this.ids && this.ids.length > 0) {
-                    this.$Modal.confirm({
-                        title: '确认删除该条数据',
-                        content: '<p>删除将无法恢复</p>',
-                        onOk: () => {
-                            this.$http.post('http://localhost:3000/users/deletes', {ids: this.ids.toString()})
-                                .then(res => {
-                                    this.$Message.info('删除成功');
-                                    this.getData();
-                                })
-                        },
-                        onCancel: () => {
-                            this.$Message.info('取消删除');
-                        }
-                    });
-                }
-                ;
-            },
-            onSelectionChange(selection){
-                if (selection && selection.length > 0) {
-                    var ids = [];
-                    var leng = selection.length;
-                    for (var i = 0; i < leng; i++) {
-                        ids.push(selection[i]._id);
-                    }
-                    ;
-                    this.ids = ids;
-                }
-                ;
-            },
-            ok () {
-                this.$Message.info('确定');
-            },
-            cancel () {
-                this.$Message.info('取消');
-            },
-            handleSubmit (name) {
-                this.$refs[name].validate((valid) => {
-                    if (valid) {
-                        
-                        if (this.formValidate._id && this.formValidate._id.length>0) {
-                            this.$http.put('http://localhost:3000/users/data/'+this.formValidate._id,this.formValidate)
-                                .then(res=>{
-                                    this.getData();
-                                    this.$Message.success('修改成功!');
-                                    this.modal = false;
-                                })
-                        }else{
-                            this.$http.post('http://localhost:3000/users/data',this.formValidate)
-                                .then(res=>{
-                                    this.getData();
-                                    this.$Message.success('提交成功!');
-                                    this.modal = false;
-                                })
-                        };
-                        
-                        
-                        
-                        
-                        console.log(this.formValidate);
-
-                    } else {
-                        this.$Message.error('表单验证失败!');
-                    }
-                })
-            },
-            handleReset (name) {
-                this.$refs[name].resetFields();
-            }
-        },
-        created(){
-//            console.log(this.filter);
-            this.getData();
-//            this.$http({
-//                url:'http://localhost:3000/users/list',
-//                method:'post',
-//                data:this.filter
-//            }).then(res=>{
-//                this.filter.list = res.data.rows;
-//            })
-        }
     }
 </script>
